49489bacf9dca00b71d233dea45ff1d614c813eb
eversDevMbini280 <eversdevmbini@gmail.com>
2026-05-28 21:25:48 +0100

    resolution du premier exercice du chapitre 2

--- 2-Structures de Controle/EXERCICES — Chapitre 2/★ Exercice FACILE/table_de_multiplication.py
+++ 2-Structures de Controle/EXERCICES — Chapitre 2/★ Exercice FACILE/table_de_multiplication.py
@@ -9,3 +9,76 @@ Indice :
 Utilisez une boucle for avec range(1, 11).
 
 """
+
+#recuperation du chiffre de l'utilisateur.
+print(
+
+    "╔═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗"
+    "\n"
+    "║ ▓▒░ bonjour cher utilisateur, obtenez la table de multiplication de votre choix en entrant n'inportequel chiffre ou nombre  ░▒▓ ║"
+    "\n"
+    "╚═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝"
+
+)
+
+
+table = int(input(
+    
+    "\n"
+    "  ▓▒░ de quelle table de multiplication avez vous besoin ? :"
+
+))
+
+
+#système de validation avec la boucle while   
+while table >= 11:
+
+    table = int(input(
+            
+        "\n"
+        "═════════════════════════════════════════════════════════════════════════════════════════════════════"
+        "\n"
+       f"  ▓▒░ erreur la table de {table} n'est pas pris en charge veuillez saisire un autre chiffre ou nombre"
+        "\n"
+        "═════════════════════════════════════════════════════════════════════════════════════════════════════"
+        "\n"
+        " "
+        "\n"
+        "  ▓▒░ de quelle table de multiplication avez vous besoin ? :"
+
+        ))
+
+    if table < 11:
+        break
+    
+
+print(
+        
+    "\n"
+    "╔══════════════════════════════════════════════════════════╗"
+    "\n"
+   f"║ ▓▒░   Voici la table de mutiplication de {table}     ░▒▓ ║"
+    "\n"
+    "╠══════════════════════════════════════════════════════════╝"
+
+)
+
+
+
+#utilisation de la bloucle for pour generer des tables de multiplication de 0 à 100
+for i in range(11):
+
+    print(
+
+        "║                                  "
+        "\n"
+       f"║ ▓▒░ {table} x {i} = {table * i}  "
+    )
+
+print(
+    
+    "║                                  "
+    "\n"
+    "╚══════════════════════════════════════════════════════════╝"
+
+)
